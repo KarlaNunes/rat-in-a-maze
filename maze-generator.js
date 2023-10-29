244326de2b7fca1961064ea15c8eee5325504424
KarlaJulyana <karla@solarz.com.br>
2023-10-29 13:24:30 -0300

    :tada: create table

---
+++ maze-generator.js
@@ -0,0 +1,85 @@
+document.addEventListener("DOMContentLoaded", function() {
+    createBlankMaze();
+    generateAnExit();
+});
+
+mazeWidth = 10;
+mazeHeight = 10;
+
+function createBlankMaze() {
+
+    let rowIndex, colIndex;
+
+    const table = document.createElement("table");
+    const tbody = document.createElement("tbody");
+
+    for (rowIndex = 1; rowIndex <= mazeHeight; rowIndex++) {
+
+        const row = document.createElement("tr");
+
+        for (colIndex = 1; colIndex <= mazeWidth; colIndex++) {
+
+            const col = document.createElement("td");
+            
+            const isStartPosition = rowIndex === 1 && colIndex === 1 
+            const isFinishPosition = rowIndex === mazeHeight && colIndex === mazeWidth
+
+            if (isStartPosition) {
+                col.style.backgroundColor = "rgb(244,0,0)";
+                col.setAttribute("type", "start");
+
+            } else if (isFinishPosition) {
+                
+                col.style.backgroundColor = "rgb(0,244,0)";
+                col.setAttribute("type", "finish");
+
+            } else {
+
+                col.style.backgroundColor = "rgb(255,255,255)";
+
+            }
+            col.setAttribute("id", "cell_" + rowIndex + "_" + colIndex);
+
+            row.appendChild(col);
+
+        }
+
+        tbody.appendChild(row);
+
+    }
+    
+    table.appendChild(tbody);
+
+    document.getElementById("maze_container").appendChild(table);
+}
+
+function generateAnExit() {
+    let currentCell;
+
+    let rowIndex = 1;
+    let colIndex = 1;
+
+    const exits = ["right", "right", "right", "right", "right", "right", "right", "right", "right", "bottom", "bottom", "bottom", "bottom", "bottom", "bottom", "bottom", "bottom", "bottom"];
+
+    for (exitIndex = 0; exitIndex < exits.length; exitIndex++) {
+
+        switch(exits[exitIndex]) {
+
+            case "right":
+
+                colIndex = colIndex + 1;
+                break;
+
+            case "bottom":
+
+                rowIndex = rowIndex + 1;
+                break;
+
+        }
+
+        currentCell = document.getElementById("cell_" + rowIndex + "_" + colIndex);
+        
+        currentCell.style.backgroundColor = "#f00000";
+
+    }
+}
