332a15a4284fb275671603b857b0ccecab8b5a5e
KarlaJulyana <karla@solarz.com.br>
2023-10-29 17:14:55 -0300

    :tada: create table from matrix

--- maze-generator.js
+++ maze-generator.js
@@ -1,90 +1,37 @@
 document.addEventListener("DOMContentLoaded", function() {
     createBlankMaze();
-    generateAnExit();
 });
 
-const mazeWidth = 10;
-const mazeHeight = 10;
 
 function createBlankMaze() {
+    const table = [
+        [0,1,0,1],
+        [0,0,0,0],
+        [0,1,0,1],
+        [0,1,0,0],
+    ]
 
-    let rowIndex, colIndex;
+    const mazeArray = table;
+    
+    const maze = document.getElementById("maze_container")
 
-    const table = document.createElement("table");
-    const tbody = document.createElement("tbody");
-
-    for (rowIndex = 1; rowIndex <= mazeHeight; rowIndex++) {
-
-        const row = document.createElement("tr");
-
-        for (colIndex = 1; colIndex <= mazeWidth; colIndex++) {
-
-            const col = document.createElement("td");
+    for (let rowIndex = 0; rowIndex < mazeArray.length; rowIndex++) {
+        let row = document.createElement("div")
+        row.classList.add("row")
+        
+        for (let column = 0; column < mazeArray[rowIndex].length; column++) {
+            let cell = document.createElement("div")
+            cell.classList.add("cell")
             
-            const isStartPosition = rowIndex === 1 && colIndex === 1 
-            const isFinishPosition = rowIndex === mazeHeight && colIndex === mazeWidth
-
-            if (isStartPosition) {
-                col.style.backgroundColor = "rgb(244,0,0)";
-                col.setAttribute("type", "start");
-
-            } else if (isFinishPosition) {
-                
-                col.style.backgroundColor = "rgb(0,244,0)";
-                col.setAttribute("type", "finish");
-
-            } else {
-
-                col.style.backgroundColor = "rgb(255,255,255)";
-
+            if (mazeArray[rowIndex][column] == 1) {
+                cell.classList.add("wall")
             }
-            col.setAttribute("id", "cell_" + rowIndex + "_" + colIndex);
 
-            row.appendChild(col);
-
+            row.appendChild(cell)
         }
 
-        tbody.appendChild(row);
-
+        maze.appendChild(row)
     }
     
-    table.appendChild(tbody);
 
-    document.getElementById("maze_container").appendChild(table);
-}
-
-function generateAnExit() {
-    let currentCell;
-
-    let rowIndex = 1;
-    let colIndex = 1;
-
-    const exits = [];
-
-    for (let exit = 1; exit < mazeWidth - 1; exit++) {
-        exits.push("right");
-        exits.push("bottom");
-    }
-
-    for (exitIndex = 0; exitIndex < exits.length; exitIndex++) {
-
-        switch(exits[exitIndex]) {
-
-            case "right":
-
-                colIndex = colIndex + 1;
-                break;
-
-            case "bottom":
-
-                rowIndex = rowIndex + 1;
-                break;
-
-        }
-
-        currentCell = document.getElementById("cell_" + rowIndex + "_" + colIndex);
-        
-        currentCell.style.backgroundColor = "#f00000";
-
-    }
 }
